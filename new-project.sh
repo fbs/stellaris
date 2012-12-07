16248eb6aeee01f84544baf929b4e0a259013435
bas smit <fbs.devv@gmail.com>
2012-12-07 23:36:09 +0100

    Updated 'new-project.sh' to make use of the new build setup.

--- new-project.sh
+++ new-project.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 DIR=$1
-echo "Copying into $1"
+echo "Creating a new project in $1"
 mkdir -p $1/src
 cp linker.ld .gitignore Makefile  $1 -r
 cp src/startup_gcc.c $1/src/
+cp src/template.x $1/src/main.c
+sed -i s/uartstdio.c// $1/Makefile
