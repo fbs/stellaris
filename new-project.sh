5bfe2f2eaa87d94f966931cd7778592a2e7d3408
bas smit <bas@baslab.org>
2012-12-08 15:53:01 +0100

    Updated new-project for the new format.

--- new-project.sh
+++ new-project.sh
@@ -2,8 +2,8 @@
 
 DIR=$1
 echo "Creating a new project in $1"
-mkdir -p $1/src
-cp linker.ld .gitignore Makefile  $1 -r
-cp src/startup_gcc.c $1/src/
-cp src/template.x $1/src/main.c
+mkdir -p $1
+git init $1
+cp linker.ld .gitignore Makefile startup_gcc.c  $1 -r
+cp template.x $1/main.c
 sed -i s/uartstdio.c// $1/Makefile
